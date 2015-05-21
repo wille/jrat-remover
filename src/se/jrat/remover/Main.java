31677f04623e88e9d39ef9d36eb019fccb99c2e2
redpois0n <red@cmail.nu>
2015-05-21 12:25:22 +0200

    Get fixer class

--- src/se/jrat/remover/Main.java
+++ src/se/jrat/remover/Main.java
@@ -6,17 +6,10 @@ import se.jrat.remover.fixers.FixLinux;
 import se.jrat.remover.fixers.FixMac;
 import se.jrat.remover.fixers.FixWin;
 import se.jrat.remover.fixers.Fixer;
-import se.jrat.remover.scanners.Scanner;
-import se.jrat.remover.scanners.ScannerLinux;
-import se.jrat.remover.scanners.ScannerMac;
-import se.jrat.remover.scanners.ScannerWin;
 
 import com.redpois0n.oslib.OperatingSystem;
 
 public class Main {
-	
-	public static Fixer fixer;
-	public static Scanner remover;
 	
 	public static final String getVersion() {
 		return "1.2";
@@ -25,19 +18,19 @@ public class Main {
 	public static void main(String[] args) throws Exception {	
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 
+		Frame frame = new Frame();
+		
+		Fixer fixer;
+
 		if (OperatingSystem.getOperatingSystem().getType() == OperatingSystem.WINDOWS) {
-			fixer = new FixWin();
-			remover = new ScannerWin();
+			fixer = new FixWin(frame);
 		} else if (OperatingSystem.getOperatingSystem().getType() == OperatingSystem.OSX) {
-			fixer = new FixMac();
-			remover = new ScannerMac();
+			fixer = new FixMac(frame);
 		} else if (OperatingSystem.getOperatingSystem().isUnix()) {
-			fixer = new FixLinux();
-			remover = new ScannerLinux();
+			fixer = new FixLinux(frame);
 		}
 		
 		Utils.info("jRAT Remover", "Please make sure to manually terminate all running Java processes before proceeding");
-		Frame frame = new Frame();
 		frame.setVisible(true);
 	}
 	
